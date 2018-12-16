841b98ea8c7a1f430e8f760214f7619708b6ef7f
Fred Wu <ifredwu@gmail.com>
2018-12-16 19:31:02 +1100

    Added `ERL_AFLAGS` for IEx history

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -13,3 +13,5 @@ export NODE_PATH=/usr/local/lib/node_modules
 export RUBY_GC_MALLOC_LIMIT=90000000
 export RUBY_GC_HEAP_FREE_SLOTS=200000
 export JRUBY_OPTS=-J-Xmx2048m
+
+export ERL_AFLAGS="-kernel shell_history enabled"
