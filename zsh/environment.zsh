f51bfd96e2d7f2ecae239955e4fabd82e97f1b28
Fred Wu <ifredwu@gmail.com>
2020-12-30 14:57:23 +1100

    Updated brew path

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -10,9 +10,4 @@ export JULIA_EDITOR=code
 
 export NODE_PATH=/usr/local/lib/node_modules
 
-export ERL_AFLAGS="-kernel shell_history enabled"
-export LDFLAGS="-L/usr/local/opt/openssl/lib"
-export CPPFLAGS="-I/usr/local/opt/openssl/include"
-
-export PKG_CONFIG_PATH="/usr/local/opt/zlib/lib/pkgconfig"
-export PATH="/usr/local/opt/openssl/bin:$PATH"
+export PATH="/opt/homebrew/bin:$PATH"
