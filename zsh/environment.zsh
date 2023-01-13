dfea0d4590ce3033a33b26676b4505cf3264d26e
Fred Wu <ifredwu@gmail.com>
2023-01-13 11:11:51 +1100

    Update envs

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -10,4 +10,10 @@ export JULIA_EDITOR=code
 
 export NODE_PATH=/usr/local/lib/node_modules
 
-export PATH="/opt/homebrew/sbin:/opt/homebrew/bin:$PATH"
+export KERL_BUILD_DOCS="yes"
+export KERL_BUILD_PLT="yes"
+export KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --enable-dynamic-ssl-lib --enable-hipe --enable-shared-zlib --enable-smp-support --enable-threads --enable-wx --with-ssl=/opt/homebrew/opt/openssl@1.1 --without-javac --without-odbc --enable-darwin-64bit --enable-kernel-poll --with-dynamic-trace=dtrace"
+
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+
+export PATH="/opt/homebrew/opt/python@3.9/libexec/bin:/opt/homebrew/opt/openjdk/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:/opt/homebrew/opt/node@18/bin:/opt/homebrew/sbin:/opt/homebrew/bin:$PATH"
