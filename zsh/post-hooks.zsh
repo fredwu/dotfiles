8553456ae8c34e7b405db646fa8000bb7afa0e0f
Fred Wu <ifredwu@gmail.com>
2024-12-13 14:39:26 +1100

    Fix brew shellenv

--- zsh/post-hooks.zsh
+++ zsh/post-hooks.zsh
@@ -1,16 +1,16 @@
 unsetopt correct
 unsetopt correct_all
 
+if command -v /opt/homebrew/bin/brew >/dev/null; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
 if command -v direnv >/dev/null; then
   eval "$(direnv hook zsh)"
 fi
 
 if command -v mise >/dev/null; then
   eval "$(mise activate zsh --shims)"
-fi
-
-if command -v brew >/dev/null; then
-  eval "$(brew shellenv)"
 fi
 
 if command -v fzf >/dev/null; then
