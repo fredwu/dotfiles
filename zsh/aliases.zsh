76e3360b8d557381f70efe5eae63e95a82518e29
Fred Wu <ifredwu@gmail.com>
2026-08-09 00:47:13 +1000

    Refactor aliases: Rename 'tt' to 't' for grok trace and adjust related alias for clarity

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -106,8 +106,8 @@ alias ss="skillshare"
 alias tf="terraform"
 alias time="command time"
 
-alias tt="grok trace --local"
-alias ttr="rm -rf ~/.grok/trace-exports/*"
+alias t="grok trace --local"
+alias tt="rm -rf ~/.grok/trace-exports/*"
 
 alias updatedb="sudo /usr/libexec/locate.updatedb"
 
